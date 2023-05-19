de58b18272d032d560d34ec21c62f58f7b0024f3
Artem Artemov <artkis@inbox.ru>
2023-05-19 11:14:00 +0300

    Это новый файл task_3.php

---
+++ task_3.php
@@ -0,0 +1,32 @@
+<?php
+
+$supportedOperators = ['+','-','*'];
+
+function calculateOperation (int $a, int $b, string $operation = '+') : int
+{
+    if ($operation == '+') {
+        return $a + $b;
+    } elseif ($operation == '-') {
+        return $a - $b;
+    } elseif ($operation == '*') {
+        return $a * $b;
+    }
+}
+function parseOperator ($userInput, $operator){
+    $parseResult = explode ($operator, $userInput);
+    if ($parseResult && count($parseResult) == 2){
+        return ['operators => $parseResult', 'operator'=> $operator];
+    }
+    return false;
+}
+
+do {
+    $userInput = readline ('введите выражение:');
+
+    foreach ($supportedOperators as $operator)
+    $parseResult = parseOperator ($operator,$userInput);
+    if ($parseResult){
+        echo 'Результат = '. calculateOperation (intval ($parseResult['operators'][0]), intval($parseResult['operators'][1]), $parseResult ['operators']);
+    }
+    
+}while (true);
