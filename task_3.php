15374fcad172207191fc7a5e2b97aa7b7025ad6c
Artem Artemov <artkis2013@yandex.ru>
2023-05-24 14:04:30 +0300

    добавлен комментарий в файл

--- task_3.php
+++ task_3.php
@@ -1,9 +1,10 @@
 <?php
-
 $supportedOperators = ['+','-','*'];
-
-function calculateOperation (int $a, int $b, string $operation = '+') : int
+$callsHistory = [];
+function calculateOperation (&$history, int $a, int $b, string $operation = '+') : int
 {
+
+    $history = $a .' ' . $operation . ' ' . $b;
     if ($operation == '+') {
         return $a + $b;
     } elseif ($operation == '-') {
@@ -12,21 +13,25 @@ function calculateOperation (int $a, int $b, string $operation = '+') : int
         return $a * $b;
     }
 }
+
 function parseOperator ($userInput, $operator){
     $parseResult = explode ($operator, $userInput);
     if ($parseResult && count($parseResult) == 2){
-        return ['operators => $parseResult', 'operator'=> $operator];
+        return ['operators' => $parseResult, 'operator'=> $operator];
     }
     return false;
 }
 
 do {
     $userInput = readline ('введите выражение:');
-
+if ($userInput == 'exit'){
+    print_r($callsHistory);
+}
     foreach ($supportedOperators as $operator)
-    $parseResult = parseOperator ($operator,$userInput);
+    $parseResult = parseOperator ($userInput, $operator);
     if ($parseResult){
-        echo 'Результат = '. calculateOperation (intval ($parseResult['operators'][0]), intval($parseResult['operators'][1]), $parseResult ['operators']);
+        echo 'Результат = '. calculateOperation ($callsHistory, intval ($parseResult['operators'][0]), intval($parseResult['operators'][1]), $parseResult ['operators']);
     }
     
-}while (true);
+}
+while (true);
